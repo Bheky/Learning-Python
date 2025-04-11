59ca7900e82f6f805203af6c9657e4e77046bbda
Bheky <127692782+Bheky@users.noreply.github.comclear>
2025-04-11 02:14:11 +0100

    Completed classes exercises + updated README file

--- Classes/car_v2.py
+++ Classes/car_v2.py
@@ -30,6 +30,29 @@ class Car:
         """Add the given amount to the odometer readings."""
         self.odometer_reading += miles
 
+
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
+
+    def __init__(self, battery_size=40):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def get_range(self):
+        """Print a statement about this battery's range."""
+        battery_range = 0
+
+        if self.battery_size == 40:
+            battery_range = 150
+        elif self.battery_size == 60:
+            battery_range = 225
+
+        print(f"This car can go approximately {battery_range} miles on a full charge.")
+
 class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles."""
 
@@ -37,21 +60,18 @@ class ElectricCar(Car):
         """Initialize attributes of parent class.
         Then initialize attributes specific to an electric car."""
         super().__init__(make, model, year)
-        self.battery_size = 40
-
-    def describe_battery(self):
-        """Print a statement describing the battery size."""
-        print(f"This car has a {self.battery_size}-kWh battery.")
+        self.battery = Battery()
 
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
-my_tesla.describe_battery()
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
 
-my_car = Car('honda', 'civic', 2015)
-print(my_car.get_descriptive_name())
+#my_car = Car('honda', 'civic', 2015)
+#print(my_car.get_descriptive_name())
 
-my_car.update_odometer(25_700)
-my_car.read_odometer()
+#my_car.update_odometer(25_700)
+#my_car.read_odometer()
 
-my_car.increment_odometer(100)
-my_car.read_odometer()
+#my_car.increment_odometer(100)
+#my_car.read_odometer()
